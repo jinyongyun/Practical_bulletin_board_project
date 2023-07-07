4f0010b97c24df41fb6ab9bf16ea78e843fb6bd7
dbswlsdyd730@naver.com <dbswlsdyd730@naver.com>
2023-07-07 17:06:43 +0900

    #8 - 도메인 클래스 설계(게시글, 댓글)
    도메인 정보를 설계해 보았다.
    기초적인 게시글과 댓글 표현이 가능하도록 함
    DB와 연동을 위한 추가적인 표현은 추후에 업데이트 될 수 있음.

---
+++ src/main/java/com/practical/practical_bulletin_board_project/domain/Article.java
@@ -0,0 +1,12 @@
+package com.practical.practical_bulletin_board_project.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title; //제목
+    private String content; //본문
+    private String hashtag; //해시태크
+
+
+}
