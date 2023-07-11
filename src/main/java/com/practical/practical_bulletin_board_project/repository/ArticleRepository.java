44ce9c7a1d6d79e2c149c57d2bf6c0dfa9765a14
dbswlsdyd730@naver.com <dbswlsdyd730@naver.com>
2023-07-11 16:00:44 +0900

    #1 - date rest 설정
    게시글, 댓글의 json api를 자동으로 restful하게 만들도록 설정

--- src/main/java/com/practical/practical_bulletin_board_project/repository/ArticleRepository.java
+++ src/main/java/com/practical/practical_bulletin_board_project/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.practical.practical_bulletin_board_project.repository;
 
 import com.practical.practical_bulletin_board_project.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
