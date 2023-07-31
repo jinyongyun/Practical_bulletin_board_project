4596854b8d3f313793205fdf0703a40517974bf4
dbswlsdyd730@naver.com <dbswlsdyd730@naver.com>
2023-07-31 15:16:53 +0900

    #24 - 잘못된 도메인 수정
    필수 요소 빠뜨리지 않게...

--- src/main/java/com/practical/practical_bulletin_board_project/service/ArticleCommentService.java
+++ src/main/java/com/practical/practical_bulletin_board_project/service/ArticleCommentService.java
@@ -1,34 +1,52 @@
 package com.practical.practical_bulletin_board_project.service;
 
+import com.practical.practical_bulletin_board_project.domain.ArticleComment;
 import com.practical.practical_bulletin_board_project.dto.ArticleCommentDto;
 import com.practical.practical_bulletin_board_project.repository.ArticleCommentRepository;
 import com.practical.practical_bulletin_board_project.repository.ArticleRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
 public class ArticleCommentService {
-
     private final ArticleRepository articleRepository;
     private final ArticleCommentRepository articleCommentRepository;
 
     @Transactional(readOnly = true)
     public List<ArticleCommentDto> searchArticleComments(Long articleId) {
-        return List.of();
+        return articleCommentRepository.findByArticle_Id(articleId)
+                .stream()
+                .map(ArticleCommentDto::from)
+                .toList();
     }
 
     public void saveArticleComment(ArticleCommentDto dto) {
+        try {
+            articleCommentRepository.save(dto.toEntity(articleRepository.getReferenceById(dto.articleId())));
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 저장 실패. 댓글의 게시글을 찾을 수 없습니다 - dto: {}", dto);
+        }
     }
 
     public void updateArticleComment(ArticleCommentDto dto) {
+        try {
+            ArticleComment articleComment = articleCommentRepository.getReferenceById(dto.id());
+            if (dto.content() != null) { articleComment.setContent(dto.content()); }
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 업데이트 실패. 댓글을 찾을 수 없습니다 - dto: {}", dto);
+        }
     }
 
     public void deleteArticleComment(Long articleCommentId) {
+        articleCommentRepository.deleteById(articleCommentId);
     }
 
 }
