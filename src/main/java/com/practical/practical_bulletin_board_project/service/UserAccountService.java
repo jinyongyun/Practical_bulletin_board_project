a87b5ab5b1fbcb24fb14e8fa6022f0b73335db68
dbswlsdyd730@naver.com <dbswlsdyd730@naver.com>
2023-08-09 14:19:44 +0900

    #45 - 회원 정보 서비스 로직의 구현
    회원 정보를 다루는 서비스 로직이 기존에 없었는데
    이번 기회에 간단한 형태로 구현함
    회원 정보의 조회와 저장을 담당

---
+++ src/main/java/com/practical/practical_bulletin_board_project/service/UserAccountService.java
@@ -0,0 +1,30 @@
+package com.practical.practical_bulletin_board_project.service;
+
+import com.practical.practical_bulletin_board_project.domain.UserAccount;
+import com.practical.practical_bulletin_board_project.dto.UserAccountDto;
+import com.practical.practical_bulletin_board_project.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class UserAccountService {
+
+private final UserAccountRepository userAccountRepository;
+
+    @Transactional(readOnly = true)
+    public Optional<UserAccountDto> searchUser(String username) {
+        return userAccountRepository.findById(username)
+                .map(UserAccountDto::from);
+    }
+
+    public UserAccountDto saveUser(String username, String password, String email, String nickname, String memo){
+        return UserAccountDto.from(
+                userAccountRepository.save(UserAccount.of(username, password, email, nickname, memo, username))
+        );
+    }
+}
