0fb7e3bf7b8733cd1f0083c4da493dd9bde59c3b
dbswlsdyd730@naver.com <dbswlsdyd730@naver.com>
2023-08-09 14:20:56 +0900

    #45 - 인증 설정 업데이트 : OAuth 인증 설정 추가
    스프링 시큐리티가 기존 디비 인증에다가 OAuth 인증까지
    받아들일 수 있도록 설정 추가
    해당 변경을 테스트에도 반영

--- src/test/java/com/practical/practical_bulletin_board_project/config/TestSecurityConfig.java
+++ src/test/java/com/practical/practical_bulletin_board_project/config/TestSecurityConfig.java
@@ -1,29 +1,39 @@
 package com.practical.practical_bulletin_board_project.config;
 
 import com.practical.practical_bulletin_board_project.domain.UserAccount;
+import com.practical.practical_bulletin_board_project.dto.UserAccountDto;
 import com.practical.practical_bulletin_board_project.repository.UserAccountRepository;
+import com.practical.practical_bulletin_board_project.service.UserAccountService;
+import org.mockito.Mock;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
 @Import(SecurityConfig.class)
 public class TestSecurityConfig {
-    @MockBean private UserAccountRepository userAccountRepository;
+    @MockBean private UserAccountService userAccountService;
 
     @BeforeTestMethod
     public void securitySetup(){
-        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .willReturn(createUserAccountDto());
+    }
 
-           "unoTest",
-           "pw",
-           "uno-test@email.com",
-           "uno-test",
-           "test memo"
-        )));
+    private UserAccountDto createUserAccountDto(){
+        return UserAccountDto.of(
+                "unoTest",
+                "pw",
+                "uno-test@email.com",
+                "uno-test",
+                "test memo"
+        );
     }
 }
